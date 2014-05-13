50d87b2c3801cc6064842fff47347d7a2e147a15
Eryldor <eryldor.dev@gmail.com>
2014-05-13 17:52:29 +0200

    Documentation

--- inscription.h
+++ inscription.h
@@ -10,6 +10,9 @@ enum Resultat
     A, B, C, D, E, F, FX, RES, EN_COURS
 };
 
+/**
+ * @brief Classe représentant une inscription à une UV
+ */
 class Inscription
 {
     const UV& m_uv;
@@ -18,12 +21,34 @@ class Inscription
 
     Inscription(const Inscription& o);
 public:
+    /**
+     * @brief Unique constructeur
+     * @param uv UV pour laquelle l'inscription est créée
+     * @param smstr Semestre de l'inscription
+     * @param res Résultat eventuel de l'inscription. Par défaut l'inscription est considérée comme EN_COURS.
+     */
     Inscription(const UV& uv, const SemestreCours &smstr, Resultat res = EN_COURS);
 
+    /**
+     * @brief Renvoie les semestre
+     * @return
+     */
     const SemestreCours& getSemestre() const { return semestre; }
+    /**
+     * @brief Renvoie l'UV
+     * @return
+     */
     const UV& getUV() const { return m_uv; }
+    /**
+     * @brief Renvoie le résultat
+     * @return
+     */
     Resultat getResultat() const {return m_resultat;}
 
+    /**
+     * @brief Modifie le résultat de l'inscription
+     * @param res
+     */
     void SetResultat(Resultat res) {m_resultat = res;}
 };
 
--- semestrecours.h
+++ semestrecours.h
@@ -16,16 +16,31 @@ struct Semestre
     unsigned short annee;
 };
 
+/**
+ * @brief Classe représentant un semestre de cours
+ */
 class SemestreCours
 {
     Semestre date;
     QVector<Inscription> inscription;
 
 public:
+    /**
+     * @brief Unique constructeur
+     * @param date
+     */
     SemestreCours(Semestre date);
 
+    /**
+     * @brief Ajoute une inscription au semestre
+     * @param i
+     */
     void ajouterInscription(const Inscription& i);
-    const QVector<Inscription> &getInscriptions();
+    /**
+     * @brief Renvoie une référence vers toutes les inscriptions du semestre
+     * @return
+     */
+    const QVector<Inscription> &getInscriptions() { return inscription; }
 };
 
 #endif // SEMESTRECOURS_H
